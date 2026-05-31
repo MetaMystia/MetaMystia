fd52b2bf9a6694419c627cf0e7ac5d304ffec8dd
MetaMiku <MetaMiku@hotmail.com>
2026-06-01 01:47:48 +0800

    poc: test spell with `any` AssetBundle

--- ResourceEx/AssetBundles/Test.cs
+++ ResourceEx/AssetBundles/Test.cs
@@ -5,15 +5,20 @@ namespace MetaMystia.ResourceEx.AssetBundles;
 [AutoLog]
 public static partial class Test
 {
+    public static GameObject TestObj;
     public static void Test1()
     {
         Log.Warning("Loading test bunble");
-        
+
         var ab = AssetBundle.LoadFromFile("M:/_testbundle");
         var allAssets = ab.LoadAllAssetsAsync<GameObject>().allAssets;
         foreach (var ase in allAssets)
         {
             Log.Warning($"Loaded asset {ase.name}");
+            if (ase.name == "_TestStar")
+            {
+                TestObj = ase.Cast<GameObject>();
+            }
         }
     }
 }
